cf943067625cb2f19f8fceb5244cb4c83fe0d37f
FridtjofLeg <fridtjof.leganger@gmail.com>
2018-11-01 13:20:36 +0100

    website 2.4
    Nettside versjon 2.4

--- Spillet/buttons.js
+++ Spillet/buttons.js
@@ -1,6 +1,6 @@
 function controls(){
-  document.getElementById('howto').src ="controls.png";
-  document.getElementById("controls").style.backgroundColor = "#E6F911";
+  document.getElementById('howto').src ="websidefiler/controls.png";
+  document.getElementById("controls").style.backgroundColor = "#2FD474";
   document.getElementById("controls").style.color = "#23272a";
   document.getElementById("items").style.backgroundColor = "#13572F";
   document.getElementById("items").style.color = "#ffffff";
@@ -8,20 +8,20 @@ function controls(){
   document.getElementById("enemies").style.color = "#ffffff";
 }
 function items(){
-  document.getElementById('howto').src ="items.png";
+  document.getElementById('howto').src ="websidefiler/items.png";
   document.getElementById("controls").style.backgroundColor = "#13572F";
   document.getElementById("controls").style.color = "#ffffff";
-  document.getElementById("items").style.backgroundColor = "#E6F911";
+  document.getElementById("items").style.backgroundColor = "#2FD474";
   document.getElementById("items").style.color = "#23272a";
   document.getElementById("enemies").style.backgroundColor = "#13572F";
   document.getElementById("enemies").style.color = "#ffffff";
 }
 function enemies(){
-  document.getElementById('howto').src ="enemies.png";
+  document.getElementById('howto').src ="websidefiler/enemies.png";
   document.getElementById("controls").style.backgroundColor = "#13572F";
   document.getElementById("controls").style.color = "#ffffff";
   document.getElementById("items").style.backgroundColor = "#13572F";
   document.getElementById("items").style.color = "#ffffff";
-  document.getElementById("enemies").style.backgroundColor = "#E6F911";
+  document.getElementById("enemies").style.backgroundColor = "#2FD474";
   document.getElementById("enemies").style.color = "#23272a";
 }
